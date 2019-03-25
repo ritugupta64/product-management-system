22790efc00dc9c307041e66f1fcf017bee3c0827
Ritu Gupta <ritugupta@ritugupta-macbookpro.roam.corp.google.com>
2019-03-25 08:55:23 +0530

    Updates

---
+++ precache-manifest.fd5285f14f619ed629e58f56ea2736fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "071e5d44c8376560395d",
+    "url": "/product-management-system/static/js/runtime~main.4a6a7fe6.js"
+  },
+  {
+    "revision": "da7fe64daa7d33ecda7e",
+    "url": "/product-management-system/static/js/main.4139fb8e.chunk.js"
+  },
+  {
+    "revision": "7728fa35518237e073f3",
+    "url": "/product-management-system/static/js/2.1690e239.chunk.js"
+  },
+  {
+    "revision": "da7fe64daa7d33ecda7e",
+    "url": "/product-management-system/static/css/main.19f77843.chunk.css"
+  },
+  {
+    "revision": "7728fa35518237e073f3",
+    "url": "/product-management-system/static/css/2.2dbb0e3c.chunk.css"
+  },
+  {
+    "revision": "549202c4e54a70b9bec7c0f7c84f4dce",
+    "url": "/product-management-system/index.html"
+  }
+];
